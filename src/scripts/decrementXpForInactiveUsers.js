1d457cc41fbbee8a1d970b142c6bab3285a68cff
Brian Morrison II <brian@brianmorrison.me>
2020-08-06 00:02:39 -0500

    Fixed issue with xp decrement script

--- src/scripts/decrementXpForInactiveUsers.js
+++ src/scripts/decrementXpForInactiveUsers.js
@@ -3,5 +3,5 @@ const xpService = require('../services/xpService')
 module.exports = {
   enabled: process.env.DECREMENT_XP_SCRIPT_ENABLED === "true",
   schedule:  "0 * * * *", // https://crontab.guru/every-1-hour
-  fn: xpService.processDecrementXpScript()
+  fn: xpService.processDecrementXpScript
 }
