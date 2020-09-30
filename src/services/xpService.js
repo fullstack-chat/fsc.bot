437a9dd43a39f67a61f548d57310b98f89b38113
DMDXV <Padfootdiamond@Gmail.com>
2020-09-30 23:34:40 +0100

    Added line back in

--- src/services/xpService.js
+++ src/services/xpService.js
@@ -126,6 +126,8 @@ exports.processDecrementXpScript = function() {
 
     if(exports.shouldDecrementXp(daysSinceContact, data[userId].penaltyCount)) {
       let decrementedXp = calculateDecrementedXp()
+      console.log(`INFO ONLY: Decrementing XP for user ${data[userId].username} from ${data[userId].currentXp} to ${decrementedXp}...`)
+      
       // data[userId].penaltyCount = decrementedXp
       if(data[userId].penaltyCount) {
         data[userId].penaltyCount++
