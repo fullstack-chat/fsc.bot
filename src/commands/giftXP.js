75795cb37f31ca814e74cfbaa12ca4313a84b862
DMDXV <Padfootdiamond@Gmail.com>
2020-11-10 03:36:20 +0000

    possible bug fixes for giftXP

--- src/commands/giftXP.js
+++ src/commands/giftXP.js
@@ -13,14 +13,12 @@ module.exports = {
 
     let newXp = args[3];
 
-    xpService.giftXP(msg, targetUser.id, targetUser.username, newXp)
-
     let isMod = await security.isMod(msg, msg.author.id);
     if (isMod) {
 
-      msg.channel.send(
-        `**${targetUser.username}** has been gifted ${xpAmount} from ${msg.author}`
-      );
+      xpService.giftXP(msg, targetUser.id, targetUser.username, newXp)
+      msg.channel.send(`**${targetUser.username}** has been gifted ${xpAmount} from ${msg.author}`);
+      
     } else {
       msg.author.send("You are not permitted to use the '!fsc give' command..");
     }
