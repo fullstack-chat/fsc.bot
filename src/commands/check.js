409bffbd940577977ebe5e24272f4c45b392a14d
DMDXV <Padfootdiamond@Gmail.com>
2020-11-10 03:15:32 +0000

    Added !fsc check command

---
+++ src/commands/check.js
@@ -0,0 +1,27 @@
+const xpService = require('../services/xpService')
+
+const helpText = `
+  Command: check
+  Description: The 'check' command can be used to fetch a users current XP and level.
+  Subcommands: none
+  Examples:
+    - Input: !fsc check
+      Output: Schwab is level 10 with 169xp
+`
+
+module.exports = {
+  command: 'check',
+  isEnabled: true,
+  helpText,
+  fn: async msg => {
+    let user = msg.content.split(' ')[2]
+
+    let currentXp = xpService.getXpForUserId(user.id)
+    if(currentXp) {
+      let currentLevel = xpService.getLevelForUserId(user.id)
+      msg.reply(`${user.username} is level ${currentLevel} with ${currentXp}xp`)
+    } else {
+      msg.reply("This user doesn't exist.")
+    }
+  }
+}
